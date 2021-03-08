b299a97f2800962ca593776067e5f782ec3900f4
andrescuello7 <andrescuellotrabajo@gmail.com>
2021-03-08 02:08:13 -0300

    actualizar get

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,27 @@
-import { Navbar, Nav, FormControl, Button, Form, NavDropdown } from 'react-bootstrap'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [texto, setTexto] = useState("");
+  const [textoTwo, setTextoTwo] = useState("");
+  const [num, setNum] = useState(0);
+  useEffect(() => {
+    Subir();
+  }, []);
+  const Subir = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:4000/api/subir")
+      setTexto(data.[num].titulo)
+      setTextoTwo(data.[num].texto)
+      setNum(num + 1)
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div>
-      Home
+    <div className="m-2">
+      <p>{texto}</p>
+      <p>{textoTwo}</p>
     </div>
   );
 };
